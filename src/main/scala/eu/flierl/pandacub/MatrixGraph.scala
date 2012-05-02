49682efc940130ab1c48e823fe301f1118916e16
Andreas Flierl <andreas@flierl.eu>
2012-05-02 21:36:44 +0200

    more work on my own silly and ugly graph

--- src/main/scala/eu/flierl/pandacub/MatrixGraph.scala
+++ src/main/scala/eu/flierl/pandacub/MatrixGraph.scala
@@ -1,6 +1,7 @@
 package eu.flierl.pandacub
 
 import scala.collection.mutable.BitSet
+import Vec.Ord
 
 class MatrixGraph(viewLen: Int) {
   private[this] val matrixLen = viewLen * viewLen
@@ -8,17 +9,23 @@ class MatrixGraph(viewLen: Int) {
   private[this] val matrix = Array.fill(matrixCells)(-1L)
   private[this] val nodes = BitSet()
   
-  def contains(v: Vec): Boolean = contains(v.x, v.y)
+  def contains(v: Vec): Boolean = nodes contains cell(v)
   
-  def contains(x: Int, y: Int): Boolean = nodes contains cell(x, y)
-  
-  def edge(mx: Vec, my: Vec): Option[Long] = {
-    val i = idx(mx, my)
-    if (i < 0 || i >= matrix.length || matrix(i) == -1L) None
-    else Some(matrix(i))
+  def add(mx: Vec, my: Vec, weight: Long): Unit = {
+    val i = idx(min(mx, my), max(mx, my))
+    matrix(i) = weight
+    nodes += i
   }
   
-  private[this] def cell(v: Vec): Int = cell(v.x, v.y)
-  private[this] def cell(x: Int, y: Int): Int = y * viewLen + x
+  def edge(mx: Vec, my: Vec): Long = {
+    val i = idx(min(mx, my), max(mx, my))
+    if (i < 0 || i >= matrix.length) -1L
+    else matrix(i)
+  }
+
+  private[this] def cell(v: Vec): Int = v.y * viewLen + v.x
+  private[this] def rev(i: Int): Vec = Vec(i % viewLen, i / viewLen)
   private[this] def idx(mx: Vec, my: Vec): Int = cell(my) * matrixLen + cell(mx)
+  private[this] def min(mx: Vec, my: Vec): Vec = if (Ord.compare(mx, my) <= 0) mx else my
+  private[this] def max(mx: Vec, my: Vec): Vec = if (Ord.compare(mx, my) > 0) mx else my
 }
